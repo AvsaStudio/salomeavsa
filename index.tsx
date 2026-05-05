42b6a1a47cfc1a6d99b3d7f890a2274d3c6ef7ba
AvsaStudio <salome.a.miller@gmail.com>
2026-05-05 11:31:22 -0400

    Remove scroll on page refresh

--- index.tsx
+++ index.tsx
@@ -1,17 +1,17 @@
 /**
  * @license
  * SPDX-License-Identifier: Apache-2.0
-*/
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 // Prevent browser from restoring previous scroll position
-if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
+if ("scrollRestoration" in history) history.scrollRestoration = "manual";
 window.scrollTo(0, 0);
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
